939548bfae95f7622ecfe014231bfd4165c362dc
rawwr <akshayace0302@gmail.com>
2022-10-31 23:39:33 +0530

    adding _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
+//general head setup
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Gotcha Events</title>
+        <meta name="description" content="get your events here" />
+        <meta name="viewpoint" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
