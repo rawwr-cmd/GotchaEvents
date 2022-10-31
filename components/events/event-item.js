4e46b2a7c86f91ca781f49b306a975490b8385ed
rawwr <akshayace0302@gmail.com>
2022-11-01 00:11:21 +0530

    optimizing images

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Button from "../ui/button";
 import classes from "./event-item.module.css";
 import AddressIcon from "../icons/address-icon";
@@ -15,7 +16,7 @@ const EventItem = ({ title, image, date, location, id }) => {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={340} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
