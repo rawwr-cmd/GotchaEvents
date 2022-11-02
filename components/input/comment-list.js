4cd6690c2800e91cb3521e8a5d7b5fd223dd2b5b
rawwr <akshayace0302@gmail.com>
2022-11-02 16:32:33 +0530

    getting data from db and showing the filtered data

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -3,7 +3,7 @@ const CommentList = ({ items }) => {
   return (
     <ul className={classes.comments}>
       {items.map((item) => (
-        <li key={item.id}>
+        <li key={item._id}>
           <p>{item.text}</p>
           <div>
             By <address>{item.name}</address>
--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,13 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   const { eventId } = req.query;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://rawwr:kCIexrkEqPj0UFbf@cluster0.eyknml1.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -16,24 +24,35 @@ const handler = (req, res) => {
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
 
-    console.log(newComment);
+    const result = await db.collection("comments").insertOne(newComment);
+    console.log(result);
+
     res.status(201).json({ message: "Added comment.", comment: newComment });
   }
 
   if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "rawwr", text: "A first comment!" },
-      { id: "c1", name: "rawwr1", text: "A first comment2!" },
-    ];
+    // const dummyList = [
+    //   { id: "c1", name: "rawwr", text: "A first comment!" },
+    //   { id: "c1", name: "rawwr1", text: "A first comment2!" },
+    // ];
 
-    res.status(200).json({ comments: dummyList });
+    //sorting by the latest
+    const documents = await db
+      .collection("comments")
+      .find({ eventId: eventId })
+      .sort({ _id: -1 }) //sort by desceonding order
+      .toArray();
+
+    res.status(200).json({ comments: documents });
   }
+
+  client.close();
 };
 
 export default handler;
